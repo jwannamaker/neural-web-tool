760930cc84264732be40d11758111d1850e497e0
Johnny Wannamaker <johnn.wannamaker@gmail.com>
2026-03-26 11:23:22 -0700

    Correction on name of python file to run

--- run.sh
+++ run.sh
@@ -4,6 +4,6 @@ cd /home/ubuntu/neural-web-tool
 
 source .venv/bin/activate
 
-pkill -f run.py || true
+pkill -f app.py || true
 
-nohup python3 run.py > output.log 2>&1 &
+nohup python3 app.py > output.log 2>&1 &
