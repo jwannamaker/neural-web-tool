750b20cf1a537ef9329cd950483c7ac6dfdf3d3d
Johnny Wannamaker <johnn.wannamaker@gmail.com>
2026-04-11 14:10:59 -0700

    Created base class for a testing suite, took notes of different learning sources, worked on more examples/tutorials
    Commit before pulling; Adding tests for Neuron.py
    
    Created src directory

--- static/js/code.js
+++ static/js/code.js
@@ -1,1 +1,11 @@
-
+async function createNetwork() {
+    const response = await fetch('/create_network', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            layer_sizes: [3, 5, 2]
+        })
+    });
+    const result = await response.json();
+    console.log(result);
+}
