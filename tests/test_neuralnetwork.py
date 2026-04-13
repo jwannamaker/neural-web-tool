6157dcfe0d3977940e2a677bdc8bc8847d83c609
Johnny Wannamaker <johnn.wannamaker@gmail.com>
2026-04-12 17:48:43 -0700

    Commit before pulling; Adding tests for Neuron.py

--- tests/test_neuralnetwork.py
+++ tests/test_neuralnetwork.py
@@ -1,8 +1,13 @@
 import pytest
 
-class TestNeuralNetwork:
-    def test_REQ_001_nn_created_with_layer_sizes(self):
-        from neuralnetwork import NeuralNetwork
-        nn = NeuralNetwork(input_size = 784, hidden_size = 16, output_size = 10)
-        assert nn.input_size == 28 * 28
+
+class TestNeuron:
+    def test_input_size(self):
+        
+
+    def test_sigmoid_function(self):
+        from neuron import Neuron
+        neuron = Neuron(
+
+
 
