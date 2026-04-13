9fcbd447a5f7490c84afb4a4c0de37f24cb43ead
tzwchen <tristanandkauai@icloud.com>
2026-04-12 18:11:33 -0700

    Implemented neuron initialization test

--- tests/test_neuralnetwork.py
+++ tests/test_neuralnetwork.py
@@ -1,13 +1,19 @@
 import pytest
-
-
+import nuimport numpy as np
+from Neuron import Neuron, Layer, NeuralNetwork
 class TestNeuron:
-    def test_input_size(self):
+    
+    def test_neuron_initialization(self):
+        """Test that the neuron initializes with correct weight and bias"""
+        num_inputs = 3
+        neuron = Neuron(num_inputs)
         
+        assert neuron.weights.shape == (num_inputs,), "Weights shape should match num_inputs"
+        assert isinstance(neuron.bias, (float, np.floating)), "Bias should be a float"
+        assert neuron.input is None, "Input should start as None"
+        assert neuron.output is None, "Output should start as None"
+    
 
-    def test_sigmoid_function(self):
-        from neuron import Neuron
-        neuron = Neuron(
 
 
 
