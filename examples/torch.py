00cabefffe8d1651f40669612c41c29561749b39
Johnny Wannamaker <johnn.wannamaker@gmail.com>
2026-04-18 08:44:13 -0700

    Delete examples/torch.py

--- examples/torch.py
+++
@@ -1,8 +0,0 @@
-"""Example using PyTorch for predicting house prices."""
-import pandas as pd
-
-data_raw = pd.read_csv('./london_houses_transformed.csv')
-
-# SEPARATE FEATURES FROM THE TARGET
-x = data_raw.drop('price', axis=1)
-y = data_raw['price']
