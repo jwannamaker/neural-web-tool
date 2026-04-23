68525dade686549ddf3626cffa5d7694790b3626
tzwchen <tristanandkauai@icloud.com>
2026-04-23 10:33:10 -0700

    Added variable types for learning rate, number of training epochs, and batch size for training.

--- cli.py
+++ cli.py
@@ -10,7 +10,7 @@ from neuralwebtool.data import Data
 
 @click.group()
 def cli() -> None:
-    """Neural Web Tool - Train and test neural networks."""
+    """Neural Web Tool - Train and test neural networks from the command line."""
     pass
 
 
@@ -19,7 +19,7 @@ def cli() -> None:
 @click.option('--activation', default='relu,relu,linear', help='Comma-separated activation functions')
 @click.option('--loss', default='cross_entropy', help='Loss function: cross_entropy, mse_loss, nll_loss')
 @click.option('--optimizer', default='adam', help='Optimizer: adam, sgd')
-@click.option('--lr', default=0.001, help='Learning rate')
-@click.option('--epochs', default=5, help='Number of training epochs')
-@click.option('--batch-size', default=32, help='Batch size for training')
+@click.option('--lr', default=0.001, type=float, help='Learning rate')
+@click.option('--epochs', default=5, type=int, help='Number of training epochs')
+@click.option('--batch-size', default=32, type=int, help='Batch size for training')
 @click.option('--save-model', default=None, help='Path to save trained model')
